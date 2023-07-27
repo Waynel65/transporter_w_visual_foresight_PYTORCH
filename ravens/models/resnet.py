aec5d8ee67b33fedc9a9a9e644abe96204f85ad1
Waynel65 <waynel@bu.edu>
2023-07-27 11:38:31 +0800

    started modifying the core model building part of VF model

---
+++ ravens/models/resnet.py
@@ -0,0 +1,293 @@
+# coding=utf-8
+# Copyright 2021 The Ravens Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Resnet module."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# import tensorflow as tf
+
+class IdentityBlock(nn.Module):
+    def __init__(self, in_channels, filters, kernel_size, include_batchnorm=False):
+        super(IdentityBlock, self).__init__()
+        filters1, filters2, filters3 = filters
+
+        self.conv1 = nn.Conv2d(in_channels, filters1, kernel_size=1)
+        self.bn1 = nn.BatchNorm2d(filters1) if include_batchnorm else nn.Identity()
+
+        self.conv2 = nn.Conv2d(filters1, filters2, kernel_size=kernel_size, padding=kernel_size//2)
+        self.bn2 = nn.BatchNorm2d(filters2) if include_batchnorm else nn.Identity()
+
+        self.conv3 = nn.Conv2d(filters2, filters3, kernel_size=1)
+        self.bn3 = nn.BatchNorm2d(filters3) if include_batchnorm else nn.Identity()
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = F.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += identity
+        out = F.relu(out)
+
+        return out
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, filters, kernel_size, stride=2, include_batchnorm=False):
+        super(ConvBlock, self).__init__()
+        filters1, filters2, filters3 = filters
+
+        self.conv1 = nn.Conv2d(in_channels, filters1, kernel_size=1, stride=stride)
+        self.bn1 = nn.BatchNorm2d(filters1) if include_batchnorm else nn.Identity()
+
+        self.conv2 = nn.Conv2d(filters1, filters2, kernel_size=kernel_size, padding=kernel_size//2)
+        self.bn2 = nn.BatchNorm2d(filters2) if include_batchnorm else nn.Identity()
+
+        self.conv3 = nn.Conv2d(filters2, filters3, kernel_size=1)
+        self.bn3 = nn.BatchNorm2d(filters3) if include_batchnorm else nn.Identity()
+
+        self.shortcut = nn.Conv2d(in_channels, filters3, kernel_size=1, stride=stride)
+        self.bn_shortcut = nn.BatchNorm2d(filters3) if include_batchnorm else nn.Identity()
+
+    def forward(self, x):
+        identity = self.shortcut(x)
+        identity = self.bn_shortcut(identity)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = F.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += identity
+        out = F.relu(out)
+
+        return out
+
+
+
+class ResNet43_8s(nn.Module):
+    def __init__(self, input_channels, output_dim, include_batchnorm=False, cutoff_early=False):
+        super(ResNet43_8s, self).__init__()
+
+        self.cutoff_early = cutoff_early
+
+        self.conv1 = nn.Conv2d(input_channels, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64) if include_batchnorm else nn.Identity()
+
+        self.stage2a = ConvBlock(64, [64, 64, 64], kernel_size=3, stride=1, include_batchnorm=include_batchnorm)
+        self.stage2b = IdentityBlock(64, [64, 64, 64], kernel_size=3, include_batchnorm=include_batchnorm)
+
+        self.stage3a = ConvBlock(64, [128, 128, 128], kernel_size=3, stride=2, include_batchnorm=include_batchnorm)
+        self.stage3b = IdentityBlock(128, [128, 128, 128], kernel_size=3, include_batchnorm=include_batchnorm)
+
+        self.stage4a = ConvBlock(128, [256, 256, 256], kernel_size=3, stride=2, include_batchnorm=include_batchnorm)
+        self.stage4b = IdentityBlock(256, [256, 256, 256], kernel_size=3, include_batchnorm=include_batchnorm)
+
+        self.stage5a = ConvBlock(256, [512, 512, 512], kernel_size=3, stride=2, include_batchnorm=include_batchnorm)
+        self.stage5b = IdentityBlock(512, [512, 512, 512], kernel_size=3, include_batchnorm=include_batchnorm)
+
+        self.stage6a = ConvBlock(512, [256, 256, 256], kernel_size=3, stride=1, include_batchnorm=include_batchnorm)
+        self.stage6b = IdentityBlock(256, [256, 256, 256], kernel_size=3, include_batchnorm=include_batchnorm)
+
+        self.stage7a = ConvBlock(256, [128, 128, 128], kernel_size=3, stride=1, include_batchnorm=include_batchnorm)
+        self.stage7b = IdentityBlock(128, [128, 128, 128], kernel_size=3, include_batchnorm=include_batchnorm)
+
+        self.stage8a = ConvBlock(128, [64, 64, 64], kernel_size=3, stride=1, include_batchnorm=include_batchnorm)
+        self.stage8b = IdentityBlock(64, [64, 64, 64], kernel_size=3, include_batchnorm=include_batchnorm)
+
+        self.stage9a = ConvBlock(64, [16, 16, output_dim], kernel_size=3, stride=1, include_batchnorm=include_batchnorm, activation=False)
+        self.stage9b = IdentityBlock(output_dim, [16, 16, output_dim], kernel_size=3, include_batchnorm=include_batchnorm, activation=False)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+
+        x = self.stage2a(x)
+        x = self.stage2b(x)
+
+        if self.cutoff_early:
+            return x
+
+        x = self.stage3a(x)
+        x = self.stage3b(x)
+
+        x = self.stage4a(x)
+        x = self.stage4b(x)
+
+        x = self.stage5a(x)
+        x = self.stage5b(x)
+
+        x = self.upsample(x)
+
+        x = self.stage6a(x)
+        x = self.stage6b(x)
+
+        x = self.upsample(x)
+
+        x = self.stage7a(x)
+        x = self.stage7b(x)
+
+        x = self.upsample(x)
+
+        x = self.stage8a(x)
+        x = self.stage8b(x)
+
+        x = self.upsample(x)
+
+        x = self.stage9a(x)
+        x = self.stage9b(x)
+
+        return x
+
+
+
+class ResNet36_4s(nn.Module):
+    def __init__(self, input_channels, output_dim, include_batchnorm=False, cutoff_early=False):
+        super(ResNet36_4s, self).__init__()
+
+        self.cutoff_early = cutoff_early
+
+        self.conv1 = nn.Conv2d(input_channels, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64) if include_batchnorm else nn.Identity()
+
+        self.conv_block1 = ConvBlock(64, [64, 64, output_dim if cutoff_early else 64], kernel_size=5 if cutoff_early else 3, stride=1, include_batchnorm=include_batchnorm)
+        self.identity_block1 = IdentityBlock(64, [64, 64, output_dim if cutoff_early else 64], kernel_size=5 if cutoff_early else 3, include_batchnorm=include_batchnorm)
+
+        if not cutoff_early:
+            self.conv_block2 = ConvBlock(64, [64, 64, 64], kernel_size=3, stride=2, include_batchnorm=include_batchnorm)
+            self.identity_block2 = IdentityBlock(64, [64, 64, 64], kernel_size=3, include_batchnorm=include_batchnorm)
+
+            self.conv_block3 = ConvBlock(64, [64, 64, 64], kernel_size=3, stride=2, include_batchnorm=include_batchnorm)
+            self.identity_block3 = IdentityBlock(64, [64, 64, 64], kernel_size=3, include_batchnorm=include_batchnorm)
+
+            self.upsample_2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+            self.conv_block4 = ConvBlock(64, [64, 64, 64], kernel_size=3, stride=1, include_batchnorm=include_batchnorm)
+            self.identity_block4 = IdentityBlock(64, [64, 64, 64], kernel_size=3, include_batchnorm=include_batchnorm)
+
+            self.upsample_3 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+            self.conv_block5 = ConvBlock(64, [16, 16, output_dim], kernel_size=3, stride=1, include_batchnorm=include_batchnorm, activation=False)
+            self.identity_block5 = IdentityBlock(64, [16, 16, output_dim], kernel_size=3, include_batchnorm=include_batchnorm, activation=False)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+
+        x = self.conv_block1(x)
+        x = self.identity_block1(x)
+
+        if self.cutoff_early:
+            return x
+
+        x = self.conv_block2(x)
+        x = self.identity_block2(x)
+
+        x = self.conv_block3(x)
+        x = self.identity_block3(x)
+
+        x = self.upsample_2(x)
+
+        x = self.conv_block4(x)
+        x = self.identity_block4(x)
+
+        x = self.upsample_3(x)
+
+        x = self.conv_block5(x)
+        x = self.identity_block5(x)
+
+        return x
+
+
+class ResNet36_4s_latent(nn.Module):
+    def __init__(self, input_channels, latent_shape, output_dim, include_batchnorm=False):
+        super(ResNet36_4s_latent, self).__init__()
+
+        self.conv1 = nn.Conv2d(input_channels, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64) if include_batchnorm else nn.Identity()
+
+        self.conv_block1 = ConvBlock(64, [64, 64, 64], kernel_size=3, stride=1, include_batchnorm=include_batchnorm)
+        self.identity_block1 = IdentityBlock(64, [64, 64, 64], kernel_size=3, include_batchnorm=include_batchnorm)
+
+        self.conv_block2 = ConvBlock(64, [64, 64, 64], kernel_size=3, stride=2, include_batchnorm=include_batchnorm)
+        self.identity_block2 = IdentityBlock(64, [64, 64, 64], kernel_size=3, include_batchnorm=include_batchnorm)
+
+        self.conv_block3 = ConvBlock(64, [64, 64, 64], kernel_size=3, stride=2, include_batchnorm=include_batchnorm)
+        self.identity_block3 = IdentityBlock(64, [64, 64, 64], kernel_size=3, include_batchnorm=include_batchnorm)
+
+        self.upsample_2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv_block4 = ConvBlock(64+latent_shape, [64, 64, 64], kernel_size=3, stride=1, include_batchnorm=include_batchnorm)  # 64+latent_shape: to accommodate for the added latent input
+        self.identity_block4 = IdentityBlock(64, [64, 64, 64], kernel_size=3, include_batchnorm=include_batchnorm)
+
+        self.upsample_3 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv_block5 = ConvBlock(64, [16, 16, output_dim], kernel_size=3, stride=1, include_batchnorm=include_batchnorm, activation=False)
+        self.identity_block5 = IdentityBlock(64, [16, 16, output_dim], kernel_size=3, include_batchnorm=include_batchnorm, activation=False)
+
+    def forward(self, x, action_input_data):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+
+        x = self.conv_block1(x)
+        x = self.identity_block1(x)
+
+        x = self.conv_block2(x)
+        x = self.identity_block2(x)
+
+        x = self.conv_block3(x)
+        x = self.identity_block3(x)
+
+        # Concatenation is done on the channel axis
+        x = torch.cat((x, action_input_data), dim=1)
+
+        x = self.upsample_2(x)
+
+        x = self.conv_block4(x)
+        x = self.identity_block4(x)
+
+        x = self.upsample_3(x)
+
+        x = self.conv_block5(x)
+        x = self.identity_block5(x)
+
+        return x
---
+++ ravens/train_pp_dynamics.py
@@ -0,0 +1,96 @@
+"""Ravens main training script for MCTS primitives."""
+
+import datetime
+import os
+from absl import app
+from absl import flags
+import numpy as np
+from ravens.dataset_pp_dynamics import DatasetPPDynamics
+from ravens.dynamics.pp_dynamics_trainer import PPDynamicsTrainer
+# import tensorflow as tf
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+
+flags.DEFINE_string('data_dir', './data_train', '')
+flags.DEFINE_string('root_dir', './', '')
+flags.DEFINE_string('model_name', 'resnet_lite', '')
+flags.DEFINE_integer('n_demos', 10, '')
+flags.DEFINE_integer('n_steps', 60000, '')
+flags.DEFINE_integer('interval', 10000, '')
+flags.DEFINE_integer('gpu', 0, '')
+flags.DEFINE_integer('n_runs', 1, '')
+flags.DEFINE_bool('debug', False, '')
+
+FLAGS = flags.FLAGS
+
+task_list = [
+  'stack-tower',
+  'stack-pyramid',  
+  'stack-square',
+  'put-block-base',
+  'stack-palace',
+  'stack-t']
+
+def main(unused_argv):
+  # Configure which GPU to use.
+  # cfg = tf.config.experimental
+  # gpus = cfg.list_physical_devices('GPU')
+  # if not gpus:
+  #   print('No GPUs detected. Running with CPU.')
+  # else:
+  #   cfg.set_visible_devices(gpus[FLAGS.gpu], 'GPU')
+
+  ## Pytorch's way to specify GPU
+  if not torch.cuda.is_available():
+    print('No GPUs detected. Running with CPU.')
+  else:
+      # Specify the GPU
+      device = torch.device(f'cuda:{FLAGS.gpu}')
+      print(f'Running with GPU: {device}')
+
+
+  # Dataset
+  dataset_list = [os.path.join(FLAGS.data_dir, f'{task}-mcts-pp-train') for task in task_list]
+  dataset = DatasetPPDynamics(dataset_list)
+
+  # Training.
+  train_dir = f'{FLAGS.root_dir}/dynamics_models/vf_{FLAGS.n_demos}'
+  for train_run in range(FLAGS.n_runs):
+    name = f'pp-dynamics-task{len(task_list)}-demo{FLAGS.n_demos}-{FLAGS.model_name}-seed{train_run}-step{FLAGS.n_steps}'
+
+    # Set up tensorboard logger for pytorch
+    curr_time = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+    log_dir = os.path.join(
+        train_dir, 
+        'logs', 
+        name,
+        curr_time, 
+        'train')
+    writer = SummaryWriter(log_dir)
+
+    # Initialize trainer.
+    np.random.seed(train_run)
+    # tf.random.set_seed(train_run)
+    torch.manual_seed(train_run)
+    trainer = PPDynamicsTrainer(
+      model_name=FLAGS.model_name,
+      repeat_H_lambda=5)
+
+    # Limit random sampling during training to a fixed dataset.
+    for i in range(len(task_list)):
+      max_demos = dataset.n_episodes_list[i]
+      assert max_demos >= FLAGS.n_demos
+      episodes = np.random.choice(range(max_demos), FLAGS.n_demos, False) # choose n_demos number of unique demos (each demo has a numbering)
+      dataset.set(i, episodes)
+    
+    # Train agent and save snapshots.
+    # wayne: the training loop is here?
+    while trainer.total_steps < FLAGS.n_steps: # n_steps by default is 60K
+      trainer.train_pp(dataset, writer=writer, debug=FLAGS.debug)
+
+      if trainer.total_steps % FLAGS.interval == 0:
+        trainer.save_to_dir(train_dir, name)
+
+if __name__ == '__main__':
+  app.run(main)
