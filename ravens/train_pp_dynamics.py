6e28f505aa829849c497c344f74e29afefef3748
Waynel65 <waynel@bu.edu>
2023-07-28 13:57:51 +0800

    trying to fix the module issues

--- ravens/train_pp_dynamics.py
+++ ravens/train_pp_dynamics.py
@@ -5,8 +5,12 @@ import os
 from absl import app
 from absl import flags
 import numpy as np
-from ravens.dataset_pp_dynamics import DatasetPPDynamics
-from ravens.dynamics.pp_dynamics_trainer import PPDynamicsTrainer
+# from ravens.dataset_pp_dynamics import DatasetPPDynamics
+# from ravens.dynamics.pp_dynamics_trainer import PPDynamicsTrainer
+
+from dataset_pp_dynamics import DatasetPPDynamics
+from dynamics.pp_dynamics_trainer import PPDynamicsTrainer
+
 # import tensorflow as tf
 import torch
 from torch.utils.tensorboard import SummaryWriter
