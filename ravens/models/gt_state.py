d4872b5413313c13ba0c2bbf865f93d5daf49e12
Waynel65 <waynel@bu.edu>
2023-07-31 22:26:56 +0800

    updated MlpModel to pytorch based

--- ravens/models/gt_state.py
+++ ravens/models/gt_state.py
@@ -15,117 +15,68 @@
 
 """MLP ground-truth state module."""
 
-import tensorflow as tf
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-layers = tf.keras.layers
-
-
-class MlpModel(tf.keras.Model):
+class MlpModel(nn.Module):
   """MLP ground-truth state module."""
+  def __init__(self, batch_size, d_obs, d_action, activation=F.relu, mdn=False, dropout=0.2, use_sinusoid=True):
+      super(MlpModel, self).__init__()
+      self.normalize_input = True
+      self.use_sinusoid = use_sinusoid
 
-  def __init__(self,
-               batch_size,
-               d_obs,
-               d_action,
-               activation="relu",
-               mdn=False,
-               dropout=0.2,
-               use_sinusoid=True):
-    super(MlpModel, self).__init__()
-    self.normalize_input = True
+      if self.use_sinusoid:
+          k = 3
+      else:
+          k = 1
 
-    self.use_sinusoid = use_sinusoid
-    if self.use_sinusoid:
-      k = 3
-    else:
-      k = 1
+      self.fc1 = nn.Linear(d_obs * k, 128)
+      self.drop1 = nn.Dropout(dropout)
+      self.fc2 = nn.Linear(128 * k, 128)
+      self.drop2 = nn.Dropout(dropout)
+      self.fc3 = nn.Linear(128 * k, d_action)
 
-    self.fc1 = layers.Dense(
-        128,
-        input_shape=(batch_size, d_obs * k),
-        kernel_initializer="normal",
-        bias_initializer="normal",
-        activation=activation)
-    self.drop1 = layers.Dropout(dropout)
-    self.fc2 = layers.Dense(
-        128,
-        kernel_initializer="normal",
-        bias_initializer="normal",
-        activation=activation)
-    self.drop2 = layers.Dropout(dropout)
-
-    self.fc3 = layers.Dense(
-        d_action, kernel_initializer="normal", bias_initializer="normal")
-
-    self.mdn = mdn
-    if self.mdn:
-      k = 26
-      self.mu = tf.keras.layers.Dense((d_action * k),
-                                      kernel_initializer="normal",
-                                      bias_initializer="normal")
-      # Variance should be non-negative, so exp()
-      self.logvar = tf.keras.layers.Dense(
-          k, kernel_initializer="normal", bias_initializer="normal")
-
-      # mixing coefficient should sum to 1.0, so apply softmax
-      self.pi = tf.keras.layers.Dense(
-          k, kernel_initializer="normal", bias_initializer="normal")
-      self.softmax = tf.keras.layers.Softmax()
-      self.temperature = 2.5
+      self.mdn = mdn
+      if self.mdn:
+          k = 26
+          self.mu = nn.Linear(128 * k, d_action * k)
+          self.logvar = nn.Linear(128 * k, k)
+          self.pi = nn.Linear(128 * k, k)
+          self.softmax = nn.Softmax(dim=1)
+          self.temperature = 2.5
 
   def reset_states(self):
-    pass
+      pass
 
   def set_normalization_parameters(self, obs_train_parameters):
-    """Set normalization parameters.
+      self.obs_train_mean = obs_train_parameters["mean"]
+      self.obs_train_std = obs_train_parameters["std"]
 
-    Args:
-      obs_train_parameters: dict with key, values:
-        - 'mean', numpy.ndarray of shape (obs_dimension)
-        - 'std', numpy.ndarray of shape (obs_dimension)
-    """
-    self.obs_train_mean = obs_train_parameters["mean"]
-    self.obs_train_std = obs_train_parameters["std"]
+  def forward(self, x):
+      def cs(input_tensor):
+          if self.use_sinusoid:
+              sin = torch.sin(input_tensor)
+              cos = torch.cos(input_tensor)
+              return torch.cat((input_tensor, cos, sin), dim=1)
+          else:
+              return input_tensor
 
-  def call(self, x):
-    """FPROP through module.
+      obs = x.clone()
 
-    Args:
-      x: shape: (batch_size, obs_dimension)
+      x = self.drop1(self.fc1(cs(obs)))
+      x = self.drop2(self.fc2(torch.cat((x, cs(obs)), dim=1)))
+      x = torch.cat((x, cs(obs)), dim=1)
 
-    Returns:
-      shape: (batch_size, action_dimension)  (if MDN)
-      shape of pi: (batch_size, num_gaussians)
-      shape of mu: (batch_size, num_gaussians*action_dimension)
-      shape of var: (batch_size, num_gaussians)
-    """
-    obs = x * 1.0
-
-    # if self.normalize_input:
-    #   x = (x - self.obs_train_mean) / (self.obs_train_std + 1e-7)
-
-    def cs(x):
-      if self.use_sinusoid:
-        sin = tf.math.sin(x)
-        cos = tf.math.cos(x)
-        return tf.concat((x, cos, sin), axis=1)
+      if not self.mdn:
+          x = self.fc3(x)
+          return x
       else:
-        return x
+          pi = self.pi(x)
+          pi = pi / self.temperature
+          pi = self.softmax(pi)
 
-    x = self.drop1(self.fc1(cs(obs)))
-    x = self.drop2(self.fc2(tf.concat((x, cs(obs)), axis=1)))
+          mu = self.mu(x)
+          var = torch.exp(self.logvar(x))
+          return (pi, mu, var)
 
-    x = tf.concat((x, cs(obs)), axis=1)
-
-    if not self.mdn:
-      x = self.fc3(x)
-      return x
-
-    else:
-      pi = self.pi(x)
-      pi = pi / self.temperature
-      pi = self.softmax(pi)
-
-      mu = self.mu(x)
-      var = tf.math.exp(self.logvar(x))
-      return (pi, mu, var)
