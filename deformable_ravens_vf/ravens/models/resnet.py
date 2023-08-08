5261b6309f73448319ff177879734215fb3c1e09
Waynel65 <waynel@bu.edu>
2023-08-08 16:32:48 +0800

    removed one upsampling in resnet

--- deformable_ravens_vf/ravens/models/resnet.py
+++ deformable_ravens_vf/ravens/models/resnet.py
@@ -153,7 +153,7 @@ class ResNet43_8s(nn.Module):
         x = self.stage5a(x)
         x = self.stage5b(x)
 
-        x = self.upsample(x)
+        # x = self.upsample(x)
 
         x = self.stage6a(x)
         x = self.stage6b(x)
