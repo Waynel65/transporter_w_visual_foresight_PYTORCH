1fc7bfb3d2f7ef493a5d3d330ed19dd3d25207da
Waynel65 <waynel@bu.edu>
2023-08-09 14:20:17 +0800

    fixing kernel error

--- deformable_ravens_vf/ravens/models/transport_goal.py
+++ deformable_ravens_vf/ravens/models/transport_goal.py
@@ -72,7 +72,7 @@ class TransportGoal:
         print(f"input shape is {input_shape}")
 
         self.model = TripleResnet(input_shape[2], output_dim)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-4)
+        self.optim = torch.optim.Adam(self.model.parameters(), lr=1e-4)
 
         # 3 fully convolutional ResNets. Third one is for the goal.
         # in0, out0 = ResNet43_8s(input_shape, output_dim, prefix='s0_')
