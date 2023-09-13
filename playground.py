00a1b42d701f44b6a9e129d0a8c92fda1735e10c
Waynel65 <waynel@bu.edu>
2023-09-13 18:03:04 -0400

    added a test file

--- playground.py
+++ playground.py
@@ -11,8 +11,8 @@ kernel_torch = torch.randn(3,36,65, 65)
 goal_x_in_logits_tf = tf.convert_to_tensor(goal_x_in_logits_torch.permute(0, 2, 3, 1).numpy()) # changing to NHWC format
 kernel_tf = tf.convert_to_tensor(kernel_torch.permute(0,2,3,1).numpy()) # to match TF's order
 
-# output_torch = torch.nn.functional.conv2d(goal_x_in_logits_torch, kernel_torch)
-output = torch.nn.functional.conv2d(goal_x_in_logits_torch, kernel_torch, groups=goal_x_in_logits_torch.shape[1])
+output_torch = torch.nn.functional.conv2d(goal_x_in_logits_torch, kernel_torch)
+# output = torch.nn.functional.conv2d(goal_x_in_logits_torch, kernel_torch, groups=goal_x_in_logits_torch.shape[1])
 
 # kernel_tf_transposed = tf.transpose(kernel_tf, [1, 2, 3, 0]) # 
 output_tf = tf.nn.convolution(goal_x_in_logits_tf, kernel_tf_transposed, data_format="NHWC")
