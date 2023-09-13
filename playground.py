0a3934cf7f7aa811a2398f41fa3990c5409a486a
Waynel65 <waynel@bu.edu>
2023-09-13 17:47:14 -0400

    added a test file

--- playground.py
+++ playground.py
@@ -2,10 +2,10 @@ import torch
 import tensorflow as tf
 import pdb
 
-pdb.set_trace()
+# pdb.set_trace()
 # PyTorch
 goal_x_in_logits_torch = torch.randn(1, 3, 224, 224)
-kernel_torch = torch.randn(36, 3, 65, 65)
+kernel_torch = torch.randn(3,36,65, 65)
 
 # TensorFlow
 goal_x_in_logits_tf = tf.convert_to_tensor(goal_x_in_logits_torch.permute(0, 2, 3, 1).numpy()) # changing to NHWC format
@@ -14,7 +14,7 @@ kernel_tf = tf.convert_to_tensor(kernel_torch.permute(0,2,3,1).numpy()) # to mat
 # output_torch = torch.nn.functional.conv2d(goal_x_in_logits_torch, kernel_torch)
 output = torch.nn.functional.conv2d(goal_x_in_logits_torch, kernel_torch, groups=goal_x_in_logits_torch.shape[1])
 
-kernel_tf_transposed = tf.transpose(kernel_tf, [1, 2, 3, 0])
+kernel_tf_transposed = tf.transpose(kernel_tf, [1, 2, 3, 0]) # 
 output_tf = tf.nn.convolution(goal_x_in_logits_tf, kernel_tf_transposed, data_format="NHWC")
 
 output_tf_torch = torch.tensor(output_tf.numpy().transpose(0, 3, 1, 2)) # Convert TF tensor to PyTorch format (NCHW)
