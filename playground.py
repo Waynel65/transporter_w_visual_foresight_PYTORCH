a3f05f052205dba0d00a6d85ebb9ab1c2d91a99f
Waynel65 <waynel@bu.edu>
2023-09-13 18:16:37 -0400

    added a test file

--- playground.py
+++ playground.py
@@ -4,7 +4,7 @@ import pdb
 
 # pdb.set_trace()
 # PyTorch
-goal_x_in_logits_torch = torch.randn(1, 3, 224, 224)
+goal_x_in_logits_torch = torch.ones(1, 3, 224, 224)
 kernel_torch = torch.randn(36,3,65, 65)
 
 # TensorFlow
@@ -12,10 +12,9 @@ goal_x_in_logits_tf = tf.convert_to_tensor(goal_x_in_logits_torch.permute(0, 2, 
 kernel_tf = tf.convert_to_tensor(kernel_torch.permute(0,2,3,1).numpy()) # to match TF's order
 
 output_torch = torch.nn.functional.conv2d(goal_x_in_logits_torch, kernel_torch)
-# output = torch.nn.functional.conv2d(goal_x_in_logits_torch, kernel_torch, groups=goal_x_in_logits_torch.shape[1])
 
-# kernel_tf_transposed = tf.transpose(kernel_tf, [1, 2, 3, 0]) # 
-output_tf = tf.nn.convolution(goal_x_in_logits_tf, kernel_tf, data_format="NHWC")
+kernel_tf_transposed = tf.transpose(kernel_tf, [1, 2, 3, 0]) # 
+output_tf = tf.nn.convolution(goal_x_in_logits_tf, kernel_tf_transposed, data_format="NHWC")
 
 output_tf_torch = torch.tensor(output_tf.numpy().transpose(0, 3, 1, 2)) # Convert TF tensor to PyTorch format (NCHW)
 diff = torch.nn.functional.mse_loss(output_torch, output_tf_torch)
